ed9d4993cbca168ee9f7e0cab497784d35026c7c
Dinesh Nagar <dineshnagarit@gmail.com>
2019-09-23 15:07:30 +0530

    Added unit test cases.

--- problem2.js
+++ problem2.js
@@ -58,4 +58,116 @@ function findDuplicateTransactions (transactions = []) {
 }
 
 
+
+  
+let assert = require("chai").assert;
+
+describe("findDuplicateTransactions()", function() {
+
+it("returns empty array if there are no transactions", function() {
+    assert.equal(findDuplicateTransactions([]), {});
+});
+
+
+it("Removes duplicates", function() {
+    assert.equal(findDuplicateTransactions(
+[
+  {
+    id: 3,
+    sourceAccount: 'A',
+    targetAccount: 'B',
+    amount: 100,
+    category: 'eating_out',
+    time: '2018-03-02T10:34:30.000Z'
+  },
+  {
+    id: 1,
+    sourceAccount: 'A',
+    targetAccount: 'B',
+    amount: 100,
+    category: 'eating_out',
+    time: '2018-03-02T10:33:00.000Z'
+  },
+  {
+    id: 6,
+    sourceAccount: 'A',
+    targetAccount: 'C',
+    amount: 250,
+    category: 'other',
+    time: '2018-03-02T10:33:05.000Z'
+  },
+  {
+    id: 4,
+    sourceAccount: 'A',
+    targetAccount: 'B',
+    amount: 100,
+    category: 'eating_out',
+    time: '2018-03-02T10:36:00.000Z'
+  },
+  {
+    id: 2,
+    sourceAccount: 'A',
+    targetAccount: 'B',
+    amount: 100,
+    category: 'eating_out',
+    time: '2018-03-02T10:33:50.000Z'
+  },
+  {
+    id: 5,
+    sourceAccount: 'A',
+    targetAccount: 'C',
+    amount: 250,
+    category: 'other',
+    time: '2018-03-02T10:33:00.000Z'
+  }
+]
+),
+ [
+   [
+    {
+      id: 1,
+      sourceAccount: "A",
+      targetAccount: "B",
+      amount: 100,
+      category: "eating_out",
+      time: "2018-03-02T10:33:00.000Z"
+    },
+    {
+      id: 2,
+      sourceAccount: "A",
+      targetAccount: "B",
+      amount: 100,
+      category: "eating_out",
+      time: "2018-03-02T10:33:50.000Z"
+    },
+    {
+      id: 3,
+      sourceAccount: "A",
+      targetAccount: "B",
+      amount: 100,
+      category: "eating_out",
+      time: "2018-03-02T10:34:30.000Z"
+    }
+  ],
+  [
+    {
+      id: 5,
+      sourceAccount: "A",
+      targetAccount: "C",
+      amount: 250,
+      category: "other",
+      time: "2018-03-02T10:33:00.000Z"
+    },
+    {
+      id: 6,
+      sourceAccount: "A",
+      targetAccount: "C",
+      amount: 250,
+      category: "other",
+      time: "2018-03-02T10:33:05.000Z"
+    }
+  ]
+]);
+});
+});
   
